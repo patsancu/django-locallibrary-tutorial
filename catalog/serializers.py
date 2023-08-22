24b3dd313915bfd92d1b454a4f7a934ab9c92758
patrick <patricksanzo@gmail.com>
2023-08-22 15:21:09 +0200

    rest: Add url field to display link to self

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -5,29 +5,29 @@ from .models import Book, Genre, Language, Author, BookInstance
 class BookSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Book
-    fields = ['id', 'title', 'author', 'summary', 'isbn', 'language', 'genre']
+    fields = ['url', 'id', 'title', 'author', 'summary', 'isbn', 'language', 'genre']
 
 
 class GenreSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Genre
-    fields = ['id', 'name']
+    fields = ['url', 'id', 'name']
 
 
 class LanguageSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Language
-    fields = ['id', 'name']
+    fields = ['url', 'id', 'name']
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Author
-    fields = ('first_name','last_name','date_of_birth','date_of_death')
+    fields = ('url', 'first_name','last_name','date_of_birth','date_of_death')
 
 
 # BookInstance
 class BookInstanceSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = BookInstance
-    fields = ('id', 'book', 'imprint', 'due_back', 'borrower', )
+    fields = ('url', 'id', 'book', 'imprint', 'due_back', 'borrower', )
