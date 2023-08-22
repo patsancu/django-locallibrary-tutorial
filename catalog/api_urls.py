e91ccf5ca7aa94cd3a2668322e2dcd44e8721d13
patrick <patricksanzo@gmail.com>
2023-08-22 15:20:45 +0200

    rest: Add openapi yml schema at /api/openapi

--- catalog/api_urls.py
+++ catalog/api_urls.py
@@ -2,9 +2,24 @@ from django.urls import path, include
 from rest_framework import routers
 from catalog import views
 
+from rest_framework.schemas import get_schema_view
+
 
 router = routers.DefaultRouter()
 router.register(r'books', views.BookViewSet)
 router.register(r'genres', views.GenreViewSet)
 router.register(r'languages', views.LanguageViewSet)
 router.register(r'authors', views.AuthorViewSet)
+
+urlpatterns = [
+    # ...
+    # Use the `get_schema_view()` helper to add a `SchemaView` to project URLs.
+    #   * `title` and `description` parameters are passed to `SchemaGenerator`.
+    #   * Provide view name for use with `reverse()`.
+    path('openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    # ...
+]
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -34,6 +34,10 @@ urlpatterns += [
     path('api/', include(api_urls.router.urls)),
 ]
 
+urlpatterns += [
+    path('api/', include(api_urls.urlpatterns))
+]
+
 
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
@@ -45,6 +49,7 @@ urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
+
 urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
