93c00a088278b46dbc66993a26f4f7938723dc1a
patrick <patricksanzo@gmail.com>
2023-08-23 19:23:30 +0200

    api: Add json schema endpoint
    /api/openapi.json
    /api/openapi.yaml
    
    And rename the yaml one

--- catalog/api_urls.py
+++ catalog/api_urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework import routers
 from catalog import views
+from rest_framework.renderers import JSONOpenAPIRenderer
 
 from rest_framework.schemas import get_schema_view
 
@@ -16,10 +17,14 @@ urlpatterns = [
     # Use the `get_schema_view()` helper to add a `SchemaView` to project URLs.
     #   * `title` and `description` parameters are passed to `SchemaGenerator`.
     #   * Provide view name for use with `reverse()`.
-    path('openapi', get_schema_view(
+    path('openapi.yaml', get_schema_view(
         title="Your Project",
         description="API for all things …",
         version="1.0.0"
-    ), name='openapi-schema'),
-    # ...
+    ), name='openapi-schema-yaml'),
+    path('openapi.json', get_schema_view(
+            title="Your Project",
+            version="1.0.0",
+            renderer_classes = [JSONOpenAPIRenderer],
+        ), name='openapi-schema-json'),
 ]
